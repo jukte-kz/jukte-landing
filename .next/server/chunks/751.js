8f7322ce5a39f8c250996c4c0e22bfe79e124871
jukte-kz <kanat.jukte@gmail.com>
2022-11-02 11:25:05 +0600

    fix: fix next version

---
+++ .next/server/chunks/751.js
@@ -0,0 +1,74 @@
+"use strict";
+exports.id = 751;
+exports.ids = [751];
+exports.modules = {
+
+/***/ 1751:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "t": () => (/* binding */ transport)
+/* harmony export */ });
+const transport = [
+    {
+        value: 1,
+        label: "Рефрижератор",
+        price: 500
+    },
+    {
+        value: 2,
+        label: "Самосвал",
+        price: 27
+    },
+    {
+        value: 3,
+        label: "Тралл",
+        price: 1000
+    },
+    {
+        value: 4,
+        label: "Изотерм",
+        price: 400
+    },
+    {
+        value: 5,
+        label: "Бортовой",
+        price: 400
+    },
+    {
+        value: 6,
+        label: "Крытый",
+        price: 400
+    },
+    {
+        value: 7,
+        label: "Открытый",
+        price: 400
+    },
+    {
+        value: 8,
+        label: "Тент",
+        price: 400
+    },
+    {
+        value: 9,
+        label: "Зерновоз",
+        price: 35
+    },
+    {
+        value: 10,
+        label: "Газель",
+        price: 180
+    },
+    {
+        value: 11,
+        label: "Мега 105",
+        price: 400
+    }
+];
+
+
+/***/ })
+
+};
+;
